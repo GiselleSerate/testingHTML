07ed0830582a8ae0b6944c1ff4cec07c9245a69c
GiselleSerate <giselle.serate@gmail.com>
2016-11-12 10:30:17 -0800

    Added css, js, php for index page. Added assets folder and added cat picture.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Awesome Webpage</title>
+</head>
+<body>
+	<div>
+		<p>This div has elements</p>
+		<img href="assets/cat.jpg"/>
+		<p>So many elements!</p>
+	</div>
+</body>
+</html>
